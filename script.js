d94645c91c10e9f8b6f8ff70fd43d263ba971f07
Kumar MM <public.kumar@gmail.com>
2023-12-11 19:11:08 -0800

    reset using new game button; all done

--- script.js
+++ script.js
@@ -11,14 +11,27 @@ const btnHold = document.querySelector(".btn--hold");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  diceEl.classList.add("hidden");
+  player0El.classList.add("player--active");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player1El.classList.remove("player--active");
+  score0El.textContent = "0";
+  score1El.textContent = "0";
+  current0El.textContent = "0";
+  current1El.textContent = "0";
+};
+
+init();
+
+btnNew.addEventListener("click", init);
 
 btnRoll.addEventListener("click", function () {
   if (playing) {
@@ -54,7 +67,7 @@ btnHold.addEventListener("click", function () {
     //   score1El.textContent = scores[1];
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    if (scores[activePlayer] > 1) {
+    if (scores[activePlayer] > 10) {
       playing = false;
       diceEl.classList.add("hidden");
       document
